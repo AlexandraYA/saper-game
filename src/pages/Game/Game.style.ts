d50f4e2462896c01b1c66a0e0d5be720ad4d045d
A.Abrosimova <auabrosimova@gmail.com>
2022-09-05 21:15:06 +0300

    Add design icons

--- src/pages/Game/Game.style.ts
+++ src/pages/Game/Game.style.ts
@@ -3,15 +3,55 @@ import { css } from "@emotion/react"
 
 const useGameStyles = (cols: number) => {
 
+  const fieldF = cols * 30 + 4 + cols - 1
+
   const field = css`
     display: grid;
     grid-template-columns: repeat(${cols}, 30px);
+    grid-template-rows: 30px;
+    box-sizing: border-box;
+    width: ${fieldF}px;
+    gap: 1px;
+    border: 2px solid #b3a588;
+  `
+
+  const fieldHeader = css`
+    display: flex;
+    justify-content: space-between;
+    width: ${fieldF}px;
+    padding: 5px;
+    border: 4px solid #b3a588;
+    border-bottom: 1px solid #b3a588;
+    background-color: #f3e2be;
+    outline-offset: -2px;
+    outline: 3px solid #f5eddb;
+    font-size: 20px;
+    line-height: 24px;
+
+    & img {
+      width: 28px;
+      height: 28px;
+      vertical-align: bottom;
+    }
+
+    & > div:first-child > span {
+      margin-left: 5px;
+    }
+
+    & > div:last-child > span {
+      margin-right: 5px;
+    }
   `
 
   const cell = css`
     width: 30px;
     height: 30px;
-    border: 1px solid gray;
+    border-right: 5px solid #b3a588;
+    outline: 1px solid #b3a588;
+    border-top: 5px solid #f3e2be;
+    border-left: 5px solid #f3e2be;
+    border-bottom: 5px solid #b3a588;
+    background-color: #edd197;
 
     &:hover {
       cursor: pointer;
@@ -20,6 +60,7 @@ const useGameStyles = (cols: number) => {
 
   return {
     field,
+    fieldHeader,
     cell
   }
 }
--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -5,12 +5,16 @@ import { settings, defaultLevel } from '../../store/data'
 import { prepareField } from '../../store/utils'
 import { TSettings, TFieldKey, TCell } from '../../store/types'
 import { useGameStyles } from './Game.style'
+import ClockIcon from '../../assets/img/clock.png'
+import MineIcon from '../../assets/img/mine.png'
 
 
 const Game: React.FC = () => {
   const { levelCode } = useParams()
   const [ field, setField ] = useState<TCell[][]>([])
   const [ curSettings, setCurSettings ] = useState<TSettings>()
+  const [ minesCounter, setMinesCounter ] = useState<number>(0)
+  const [ timer, setTimer ] = useState<number>(0)
 
   const style = useGameStyles(curSettings?.cols ? curSettings.cols : settings[defaultLevel].cols)
 
@@ -20,17 +24,27 @@ const Game: React.FC = () => {
       ? settings[levelCode as TFieldKey] : settings[defaultLevel]
 
     setCurSettings(_curSettings)
+    setMinesCounter(_curSettings?.mines)
     setField(prepareField(_curSettings))
   },[levelCode])
 
 
   return (
     <div>
-      <div>Бомб {curSettings?.mines}</div>
+      <div css={style.fieldHeader}>
+        <div>
+          <img src={MineIcon} alt="flag"/>
+          <span>{minesCounter}</span>
+        </div>
+        <div>
+          <span>{timer}</span>
+          <img src={ClockIcon} alt="timer"/>
+        </div>
+      </div>
       <div css={style.field}>
         {field.map((row: TCell[], ind: number) => {
           return row.map((cell: TCell, colInd: number) => (
-            <div key={ind+colInd} css={style.cell}>{cell.indicator}</div>
+            <div key={ind+colInd} css={style.cell}></div>
           ))
         })}
       </div>
